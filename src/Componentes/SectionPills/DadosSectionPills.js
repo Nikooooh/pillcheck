a3f6c794306671d5ecdf947b127fbc3a77395be2
Nikooooh <nicolastrevis@hotmail.com>
2023-08-14 22:11:38 -0300

    Adicionando Descrição para alguns itens

--- src/Componentes/SectionPills/DadosSectionPills.js
+++
@@ -1,5 +0,0 @@
-import Remedio from '../../imagens/remedio1.jpg';
-
-export const remedios = [ {nome:'Cloridrato de Ondansetrona 8mG ', id:1, src:Remedio},
-{nome:'Cloridrato de Ondansetrona 8mG ', id:2, src:Remedio}, {nome:'Cloridrato de Ondansetrona 8mG ', id:3, src:Remedio},{nome:'Cloridrato de Ondansetrona 8mG ', id:4, src:Remedio}
-,{nome:'Cloridrato de Ondansetrona 8mG ', id:5, src:Remedio}]
